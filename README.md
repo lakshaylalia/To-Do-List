969c6fa0d1f91e22faf65776292ef81bb5b73987
Lakshay Lalia <lakshaylalia@gmail.com>
2024-06-30 20:04:26 +0530

    Added Basic design and structure of ToDo List

---
+++ README.md
@@ -0,0 +1,2 @@
+# To Do List
+Basic To Do list created using HTML , CSS and JS
---
+++ app.js
@@ -0,0 +1,11 @@
+let body=document.querySelector("body");
+let login=document.getElementById("loginBtn")
+let sign=document.getElementById("signupBtn")
+
+let task=document.querySelector("input");
+let add=document.getElementById("addBtn")
+let reset=document.getElementById("resetBtn")
+
+add.addEventListener("click",()=>{
+    console.log(task)
+})
